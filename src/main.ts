8aef87a1e384037ffe22202c6dc9288f9d052016
Hudson-Blankner <hudson.blankner@colorado.edu>
2026-03-31 12:53:41 -0600

    A

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ const canvasHeight = 500;
 
 let masterArray = [1, 2, 3, 4, 5, 6, 7 ,8 , 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 let gapSet = 5
-let widthLimiter = (canvasWidth - 100) / masterArray.length - 5
+let widthLimiter = (canvasWidth - 100) / masterArray.length - gapSet
 // widthLimiter = 5
 let widthGap = widthLimiter + gapSet
 // widthLimiter -= 1
