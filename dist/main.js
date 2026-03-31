f6a9186ffb7ecb892d8adc34072ed47ab49ddaef
Hudson-Blankner <hudson.blankner@colorado.edu>
2026-03-31 11:23:27 -0600

    setup
    Co-authored-by: ridgedicarlo <ridgedicarlo@users.noreply.github.com>

--- dist/main.js
+++ dist/main.js
@@ -1,3 +1,13 @@
 console.log("working");
+const canvas = document.getElementById("sortCanvas");
+if (!(canvas instanceof HTMLCanvasElement)) {
+    throw new Error("Canvas not found");
+}
+const ctx = canvas.getContext("2d");
+if (!ctx) {
+    throw new Error("Could not get 2D context");
+}
+ctx.fillStyle = "skyblue";
+ctx.fillRect(10, 10, 40, 400);
 export {};
 //# sourceMappingURL=main.js.map
