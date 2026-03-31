987c0243ee071ae5ad05512e41406a0b59453e6b
Hudson-Blankner <hudson.blankner@colorado.edu>
2026-03-31 12:08:30 -0600

    A

--- dist/main.js
+++ dist/main.js
@@ -7,7 +7,20 @@ const ctx = canvas.getContext("2d");
 if (!ctx) {
     throw new Error("Could not get 2D context");
 }
-ctx.fillStyle = "skyblue";
-ctx.fillRect(100, 200, 150, 150);
+const canvasWidth = 1000;
+const canvasHeight = 500;
+let masterArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+let widthLimiter = (canvasWidth - 100) / masterArray.length;
+console.log(widthLimiter);
+// ctx.fillStyle = "skyblue";
+// ctx.fillRect(10, 10, 150, 150);
+function drawBoxes(x, y, w, h) {
+    ctx.fillStyle = "#1312128d";
+    let widthGap = w + 10;
+    for (let i = 0; i < masterArray.length; i++) {
+        ctx.fillRect(x + i * widthGap, y, w, h);
+    }
+}
+drawBoxes(50, 450, widthLimiter, -50);
 export {};
 //# sourceMappingURL=main.js.map
